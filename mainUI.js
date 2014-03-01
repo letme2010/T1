b973541dbe75f500cadbd4c3e4014ebdb589f434
letme2010 <letme2010@gmail.com>
2014-03-01 21:29:58 +0800

    make name of Android YunicaTovah

--- mainUI.js
+++ mainUI.js
@@ -1,5 +1,7 @@
 $('#send').click(onSendBtnClick);
 
+var SERVER_HOST = "127.0.0.1:9527";
+
 function onSendBtnClick() {
 
 	try {
@@ -13,7 +15,7 @@ function onSendBtnClick() {
 }
 
 function requestServer() {
-	var url = "http://127.0.0.1:9527";
+	var url = "http://" +SERVER_HOST;
 	$.get(url, function(data) {
 
 		try {
@@ -35,29 +37,45 @@ function requestServer() {
 var count = 0;
 
 function node2JQueryObj(aNode) {
-	var _jqDiv = $('<div></div>');
-
+	var _jqDiv = $('<div class="view-root"></div>');
+        var _jqChildrenContainor = $('<div class="view-children-containor"></div>');
+        var _jqSnap = $('<img/>');
+        
+        _jqDiv.append(_jqSnap).append(_jqChildrenContainor);
+        _jqDiv.addClass(aNode.class);
 	_jqDiv.css({
-		"border" : "0px solid black",
-		"background-color" : getRandomColorChar(),
-		"position" : "absolute"
+//		"border" : "1px solid black",
+		"border" : "0px"
+//		,"background-color" : getRandomColorChar(),
+		,"position" : "absolute"
 	});
+        
+        _jqChildrenContainor.css({
+            "width" : "100%"
+            ,"height" : "100%"
+//            ,"background-color" : "red"
+        });
 
 	_jqDiv.offset({
-		'top' : aNode.top,
-		'left' : aNode.left
+		'top' : aNode.ltrb[1],
+		'left' : aNode.ltrb[0]
 	});
-
-	_jqDiv.html(count++);
-
-	_jqDiv.width(aNode.right - aNode.left);
-	_jqDiv.height(aNode.bottom - aNode.top);
+        
+	_jqDiv.width(getWidth(aNode));
+	_jqDiv.height(getHeight(aNode));
 
 	for (var i = 0, len = aNode.children.length; i < len; i++) {
-		var _jqChild = node2JQueryObj(aNode.children[i]);
-		console.log(aNode.children[i]);
-		_jqDiv.append(_jqChild);
+            var _oChildNode =aNode.children[i];
+            if ((0 >= getWidth(aNode)) || 0 >= getHeight(aNode)) {
+                continue;
+            }
+            var _jqChild = node2JQueryObj(_oChildNode);
+            _jqChildrenContainor.append(_jqChild);
 	}
+        
+        var imgPath = "http://" + SERVER_HOST + "/viewSnap/" + aNode.hashCode + ".png";
+        console.log(imgPath);
+        _jqSnap.attr('src', imgPath);
 
 	return _jqDiv;
 
@@ -73,7 +91,15 @@ function getRandomColorChar() {
 	return ret;
 }
 
-var sColorChars = "0123456789ABCDEF"
+var sColorChars = "0123456789ABCDEF";
+
+function getWidth(aNode) {
+    return aNode.ltrb[2] - aNode.ltrb[0];
+}
+
+function getHeight(aNode) {
+    return aNode.ltrb[3] - aNode.ltrb[1];
+}
 
 function random() {
 	return Math.floor(Math.random(16) * 16);
---
+++ run.sh
@@ -0,0 +1,2 @@
+sudo killall node-webkit
+zip T1.nw index.html package.json jquery.js main.js mainUI.js
